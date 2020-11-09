74578782d04974b59e6e8d621ca38e5a9770a2a6
Alexandre Meneghello <sg0207087@JAMB07HG8WL.local>
2020-11-09 08:31:48 -0300

    added readme.md content

--- com-sabre-redapp-showcase-web-module/src/code/views/cmdHelper/gdsData/CommandServicePopover.tsx
+++ com-sabre-redapp-showcase-web-module/src/code/views/cmdHelper/gdsData/CommandServicePopover.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Button } from "../../../components/Button";
 import { Checkbox } from "../../../components/Checkbox";
 import { Input } from "../../../components/Input";
+import { Payload } from "../../../components/Payload";
 import { PopoverForm } from "../../../components/PopoverForm";
 import { getService } from "../../../Context";
 import { CommFoundHelper } from "../../../services/CommFoundHelper";
@@ -16,6 +17,7 @@ export interface CommandServicePopoverState {
     formatToExecute:string;
     showRq:boolean;
     showRs:boolean;
+    cmdResponse:any;
 }
 export class CommandServicePopover extends React.Component<CommandServicePopoverProps,CommandServicePopoverState> {
 
@@ -29,7 +31,8 @@ export class CommandServicePopover extends React.Component<CommandServicePopover
     state : CommandServicePopoverState = {
         formatToExecute:"",
         showRq:true,
-        showRs:true
+        showRs:true,
+        cmdResponse:null
     }
     handleChange(e) : void {
         this.setState({[e.target.name]:e.target.value});
@@ -38,13 +41,28 @@ export class CommandServicePopover extends React.Component<CommandServicePopover
         this.setState({[e.target.name]:!this.state[e.target.name]});
     }
     handleExecute() :void {
-        getService(CommFoundHelper).sendCommandMessage(this.state.formatToExecute,this.state.showRq,this.state.showRs).then((res)=>{this.props.handleClose()});
+        getService(CommFoundHelper)
+            .sendCommandMessage(
+                this.state.formatToExecute,
+                this.state.showRq,
+                this.state.showRs
+            ).then(
+                (res)=>{
+
+                    if(this.state.showRs || this.state.showRs){
+                        this.props.handleClose();
+                    }else{
+                        this.setState({cmdResponse:res});
+                    }
+                    
+                }
+            );
 
     }
 
     renderButtons(): JSX.Element[] {
         return(
-            [<Button name="btnCancel" title="Cancel" handleClick={this.props.handleClose}/>,<Button name="btnExecute" title="Execute" handleClick={this.handleExecute}/>]
+            [<Button name="btnCancel" title="Cancel" type="cancel" handleClick={this.props.handleClose}/>,<Button name="btnExecute" title="Execute" type="primary" handleClick={this.handleExecute}/>]
         );
     }
     render(): JSX.Element {
@@ -53,7 +71,10 @@ export class CommandServicePopover extends React.Component<CommandServicePopover
                 <Input type="text" name="formatToExecute" title="Format" placeHolder="enter Sabre GDS Format"  value={this.state.formatToExecute} handleChange={this.handleChange}/>
                 <Checkbox disabled={false} name="showRq" title="Show Command" placeHolder="to show or not"  value={this.state.showRq} handleChange={this.handleCheck}/>
                 <Checkbox disabled={false} name="showRs" title="Show Response" placeHolder="to show or not"  value={this.state.showRs} handleChange={this.handleCheck}/>
-
+                {this.state.cmdResponse!=null?
+                    <Payload name="plResult" type="json" value={JSON.stringify(this.state.cmdResponse)} title="Response payload" />:
+                    null
+                }
             </PopoverForm>
         );
     }
